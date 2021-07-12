dde576cca35ea2eed9390c837578d69abd878e8d
StrangeOne101 <strange.toby@gmail.com>
2021-07-12 22:27:56 +1200

    .14-1.16: Auto stash before cherry pick of "Skip invalid height "

--- src/main/java/com/volmit/iris/manager/command/world/CommandIrisCreate.java
+++ src/main/java/com/volmit/iris/manager/command/world/CommandIrisCreate.java
@@ -2,6 +2,7 @@ package com.volmit.iris.manager.command.world;
 
 import com.volmit.iris.Iris;
 import com.volmit.iris.IrisSettings;
+import com.volmit.iris.generator.IrisWorldManager;
 import com.volmit.iris.manager.IrisDataManager;
 import com.volmit.iris.manager.link.MultiverseCoreLink;
 import com.volmit.iris.object.IrisDimension;
@@ -14,6 +15,11 @@ import org.bukkit.World;
 import org.bukkit.WorldCreator;
 
 import java.io.File;
+import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class CommandIrisCreate extends MortarCommand
 {
@@ -27,7 +33,56 @@ public class CommandIrisCreate extends MortarCommand
 
 	@Override
 	public void addTabOptions(MortarSender sender, String[] args, KList<String> list) {
+		if (args.length == 0 || args[args.length - 1].equals("")) { //They are about to type a new argument
+			list.addAll(getBase(args));
+			return;
+		}
 
+		String[] split = args[args.length - 1].split("\\Q=\\E");
+		if (split.length == 0) { //They haven't typed the = yet so just keep the options there
+			list.addAll(getBase(args));
+			return;
+		}
+
+		String pre = split[0].toLowerCase();
+
+		if (pre.equals("type")) {
+			for (String s : Iris.proj.getListing(true).keySet()) {
+				list.add("type=" + s);
+			}
+			if (!list.contains("type=overworld")) {
+				list.contains("type=overworld");
+			}
+		} else if (pre.equals("seed")) {
+			list.add("seed=1337");
+			list.add("seed=" + new Random().nextInt());
+			list.add("seed=random");
+		} else if (pre.equals("pregen")) {
+			list.add("500");
+			list.add("1000");
+			list.add("2000");
+			list.add("5k");
+			list.add("10k");
+			list.add("25k");
+		}
+	}
+
+	private KList<String> getBase(String[] args) {
+		KList<String> list = new KList<>();
+		boolean seed = true;
+		boolean type = true;
+		boolean pregen = true;
+
+		for (String s : args) {
+			if (s.toLowerCase().startsWith("seed=")) seed = false;
+			else if (s.toLowerCase().startsWith("type=")) type = false;
+			else if (s.toLowerCase().startsWith("pregen=")) pregen = false;
+		}
+
+		if (seed) list.add("seed=");
+		if (type) list.add("type=");
+		if (pregen) list.add("pregen=");
+		return list;
 	}
 
 	@Override
@@ -38,24 +93,83 @@ public class CommandIrisCreate extends MortarCommand
 			sender.sendMessage("/iris create <NAME> [type=overworld] [seed=1337] [pregen=5000]");
 			return true;
 		}
-
+		Random random = new Random();
 		String worldName = args[0];
 		String type = IrisSettings.get().getGenerator().getDefaultWorldType();
-		long seed = 1337;
-		int pregen = 0;
+		long seed = random.nextLong(); //Random seed when creating a world
+		AtomicInteger pregen = new AtomicInteger(256);
 		boolean multiverse = Iris.linkMultiverseCore.supported();
 
 		for(String i : args)
 		{
 			type = i.startsWith("type=") ? i.split("\\Q=\\E")[1] : type;
-			seed = i.startsWith("seed=") ? Long.valueOf(i.split("\\Q=\\E")[1]) : seed;
-			pregen = i.startsWith("pregen=") ? getVal(i.split("\\Q=\\E")[1]) : pregen;
+			seed = i.startsWith("seed=") ? (i.split("\\Q=\\E")[1].equalsIgnoreCase("random") ? random.nextLong() : Long.valueOf(i.split("\\Q=\\E")[1])) : seed;
+			pregen.set(i.startsWith("pregen=") ? getVal(i.split("\\Q=\\E")[1]) : pregen.get());
 		}
 
 		Iris.linkMultiverseCore.assignWorldType(worldName, type);
 		World world = null;
 		IrisDimension dim;
 		File folder = new File(worldName);
+
+		Runnable onDone = () -> {
+
+			sender.sendMessage(worldName + " Spawn Area generated.");
+			sender.sendMessage("You must remember to either have multiverse installed or use the Bukkit method, otherwise the world will go corrupt!");
+			sender.sendMessage("Wiki: https://volmitsoftware.gitbook.io/iris/getting-started");
+
+			O<Boolean> b = new O<Boolean>();
+			b.set(true);
+
+			if(sender.isPlayer())
+			{
+				try
+				{
+					sender.player().teleport(world.get().getSpawnLocation());
+				}
+
+				catch(Throwable e)
+				{
+
+				}
+			}
+
+			if(pregen.get() > 0)
+			{
+				b.set(false);
+				int size = pregen.get();
+				size *= 2;
+				sender.sendMessage("Pregenerating " + worldName + " " + size + " x " + size);
+				sender.sendMessage("Expect server lag during this time. Use '/iris pregen stop' to cancel");
+
+				new Pregenerator(world.get(), size, () ->
+				{
+					b.set(true);
+				});
+			}
+
+			World ww = world.get();
+			if (ww == null){
+				sender.sendMessage("World not created, can not finish");
+				return;
+			}
+			J.a(() ->
+			{
+				while(!b.get())
+				{
+					J.sleep(1000);
+				}
+
+
+				Bukkit.getScheduler().scheduleSyncDelayedTask(Iris.instance, () ->
+				{
+					ww.save();
+					sender.sendMessage("All Done!");
+				});
+			});
+		};
+
+
 		if(multiverse)
 		{
 			dim = IrisDataManager.loadAnyDimension(type);
@@ -83,7 +197,6 @@ public class CommandIrisCreate extends MortarCommand
 			Bukkit.dispatchCommand(sender, command);
 			world= Bukkit.getWorld(worldName);
 		}
-
 		else
 		{
 			if(folder.exists())
@@ -130,39 +243,14 @@ public class CommandIrisCreate extends MortarCommand
 
 			world = wc.createWorld();
 
-			done.set(true);
-		}
+				World w = INMS.get().createWorld(wc);
+				world.set(w);
 
+				J.a(() -> {
+					new Pregenerator(w, pregen.get() * 2);
+				});
 
-		sender.sendMessage(worldName + " Spawn Area generated.");
-		sender.sendMessage("You must remember to either have multiverse installed or use the Bukkit method, otherwise the world will go corrupt!");
-		sender.sendMessage("Wiki: https://volmitsoftware.gitbook.io/iris/getting-started");
-
-		O<Boolean> b = new O<Boolean>();
-		b.set(true);
-
-		if(sender.isPlayer())
-		{
-			try
-			{
-				sender.player().teleport(world.getSpawnLocation());
-			}
-
-			catch(Throwable e)
-			{
-
-			}
-		}
-
-		if(pregen > 0)
-		{
-			b.set(false);
-			sender.sendMessage("Pregenerating " + worldName + " " + pregen + " x " + pregen);
-			sender.sendMessage("Expect server lag during this time. Use '/iris pregen stop' to cancel");
-
-			new Pregenerator(world, pregen, () ->
-			{
-				b.set(true);
+				done.set(true);
 			});
 		}
 
